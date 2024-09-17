7d5c06325890f79c6c7e0f07dad1a0ff4c22bcce
Fernando Custodio <fwcustodio@gmail.com>
2024-09-17 16:56:59 -0400

    Ajuste conexão mongo

--- api/src/db.ts
+++ api/src/db.ts
@@ -5,10 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI ||
-        "mongodb+srv://apptoquin:afkqb1sq9NaCnJ5C@cluster0.r0orp.mongodb.net/data_base_teste_17-09-2024"
-    );
+    await mongoose.connect(process.env.MONGO_URI || "");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
